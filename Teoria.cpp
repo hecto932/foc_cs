6bbbc13d04f33eb8bca98c90dd8af7928062cba0
Hector Flores <hecto932@gmail.com>
2014-04-23 13:52:02 -0430

    Parte teorica completada, valores corroborados con kiara

--- Teoria.cpp
+++ Teoria.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+//CONSTRUCTOR
 Teoria::Teoria()
 {
 	L 	= 0;
@@ -16,6 +17,7 @@ Teoria::Teoria()
 	Ps 	= 0;
 }
 
+//CALCULA EL FACTORIAL DE UN NUMERO
 long long int Teoria::factorial(long long int numero)
 {
 	long long int resultado=1;
@@ -23,76 +25,69 @@ long long int Teoria::factorial(long long int numero)
 		resultado*=i;
 	return resultado;
 }
+
+//CALCULA LA POTENCIA (Base, Exponente)
 double Teoria::potencia(double base,double exponente)
 {
 	return pow(base,exponente);
 }
+
+//INICIALIZA EL MODELO CALCULANDO CIERTOS PARAMETROS
 void Teoria::Inicializar(double Lambda, double Miu)
 {
-	//double Pc=0;//P0
-	//this->Ro=Lambda/Miu;
-	//Obtenemos la potencia de Ro necesaria para la sumatoria
-	//for (int i=0;i<4;i++)
-	//{
-	//	Pc+=((potencia(this->Ro,i))/factorial(i));
-	//}
-	//Pc+=((potencia(this->Ro,4))/(24*(1-(this->Ro/4))));
-	//this->Po=1/Pc;
-	//this->Ps=(potencia(this->Ro,4)/24)*this->Po;
-
 	double Pc = 0;
-
-	Ro = Lambda/Miu;
-
+	Ro = Lambda/Miu;//CALCULAMOS RO
 	for(int i=0;i<4;i++)
-		Pc += (potencia(Ro,i)/factorial(i));
+		Pc += (potencia(Ro,i)/factorial(i)); 
 	Pc += ( potencia(Ro, 4) / ( 24 * ( 1 - Ro/4 ) ) );
 	Po = potencia(Pc,-1);
 	Ps = ( potencia(Ro,4) / 24 ) * Po;
 }
 
+//RESOLVEMOS PARTE DEL MODELO 
 void Teoria::Resolver(double Lambda, double Miu)
 {
-	//this->Lq=((4*this->Ro)/(potencia((4-this->Ro),2)))*this->Ps;
-	//this->L=this->Ro+this->Lq;
-	//this->Wq=this->Lq/Lambda;
-	//this->W=this->Wq + (1/Miu);
-	//this->X=((this->L-this->Lq)/4)*100;
-
 	Lq = ( (4 * Ro) / potencia((4 - Ro),2) ) * Ps;
+	
 	L = Lq + Ro;
+	
 	Wq = Lq / Lambda;
+	
 	W = Wq + (1/Miu);
+	
 	X = ( (L - Lq) / 4 ) * 100; 
 }
+
+//METODOS GET
 double Teoria::Get_L()
 {
-	return this->L;
+	return L;
 }
 double Teoria::Get_Lq()
 {
-	return this->Lq;
+	return Lq;
 }
 double Teoria::Get_W()
 {
-	return this->W;
+	return W;
 }
 double Teoria::Get_Wq()
 {
-	return this->Wq;
+	return Wq;
 }
 double Teoria::Get_X()
 {
-	return this->X;
+	return X;
 }
+//METODO LIMPIAR
 void Teoria::limpiar()
 {
-	this->L 	= 0;
-	this->Lq 	= 0;
-	this->Wq 	= 0;
-	this->W 	= 0;
-	this->X 	= 0;
-	this->Ro 	= 0;
-	this->Po 	= 0;
-	this->Ps 	= 0;
+	L 	= 0;
+	Lq 	= 0;
+	Wq 	= 0;
+	W 	= 0;
+	X 	= 0;
+	Ro 	= 0;
+	Po 	= 0;
+	Ps 	= 0;
 }
--- main.cpp
+++ main.cpp
@@ -38,12 +38,12 @@ int main(){
 			//cout << a << " " << b << " " << cr << " " << tt << " " << ts << endl;
   		 	
 		 	//Inicio de la parte Teorica:
-			T.Inicializar(a,b);
-			T.Resolver(a,b);
+			T.Inicializar(1/a,1/b);
+			T.Resolver(1/a,1/b);
 			std::cout << std::fixed; 
 			cout << "Modelo " << i+1 << ":" << endl;
 			cout << "\t\t\t" << "Utilización" << "\t\t" << "L" << "\t\t"  << "Lq" << "\t\t" << "W" << "\t\t" << "Wq" << endl;
-			cout << "Teórico" << "\t\t" << setprecision(2) << T.Get_X() << "\t\t\t" << setprecision(2) << T.Get_L() << "\t" << setprecision(2) << T.Get_Lq() << "\t3" << setprecision(2) << T.Get_W() << "\t4" << setprecision(2) << T.Get_Wq() << endl;
+			cout << "Teórico" << "\t\t" << setprecision(2) << T.Get_X() << "\t\t\t" << setprecision(2) << T.Get_L() << "\t" << setprecision(2) << T.Get_Lq() << "\t" << setprecision(2) << T.Get_W() << "\t" << setprecision(2) << T.Get_Wq() << endl;
 			T.limpiar();
 			cout << endl;
 		}
