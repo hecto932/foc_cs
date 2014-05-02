a3a8c638402096b7c93e6f3570a7ac71b7aff72a
Gabriel Tovar <gabrieltovar90@gmail.com>
2014-05-02 01:35:09 -0430

    Adelanto de simulacion, conflictos existentes en salidas, requiere revision exaustiva, SI SE EJECUTA DARA CICLO INFINITO

--- Simulacion.h
+++ Simulacion.h
@@ -24,8 +24,8 @@ class Simulacion
 	public:
 	 	//CONSTRUCTOR
 	 	Simulacion();
-	 	bool comprobarSig_estado(vector<double> &t_salidas, double &siguiente_salida, double siguiente_entrada, double ts);
-
+	 	//DETECTOR DE ESTADOS DENTRO DE LA SIMULACION
+	 	bool comprobarSig_estado(vector<double> &t_salidas, double &siguiente_salida, double siguiente_entrada, double ts, int Nt);
 	 	//EJECUCIÓN DE LA SIMULACIÓN
 	 	void ejecutar(double a, double b, double replicas, double tTrans, double tSim);
 	 	//OBSERVADORES
@@ -50,23 +50,30 @@ Simulacion::Simulacion()
 }
 
 //COMPARE DEL MIN_ELEMENT
-bool compare(int i, int j)
+bool compare(double i, double j)
 {
-	if(i==j && i ==0 && j==0)
-		return 0;
-	else if(i<j)
-		return i;
+	return (i<j && i!=0 && j!=0);
 }
 
-//FUNCION QUE NOS AYUDA A ENCONTRAR LA POSICION PARA ALMACENAR tSalida Y CONOCER EL SIGUIENTE EVENTO
-bool Simulacion::comprobarSig_estado (vector<double> &t_salidas, double &siguiente_salida, double siguiente_entrada, double ts)
+//FUNCION QUE NOS AYUDA A ENCONTRAR LA POSICION PARA ALMACENAR siguiente_salida EN t_salidas Y CONOCER EL SIGUIENTE EVENTO
+bool Simulacion::comprobarSig_estado (vector<double> &t_salidas, double &siguiente_salida, double siguiente_entrada, double ts, int Nt)
 {
 	int min_position = min_element(t_salidas.begin(),t_salidas.end(), compare)-t_salidas.begin();	//OBTENGO LA POSICION DEL MENOR TIEMPO
-	t_salidas[min_position] = siguiente_salida;												//REGISTRO EL NUEVO TIEMPO DE SALIDA
-	siguiente_salida = *min_element(t_salidas.begin(),t_salidas.end(), compare);						//OBTENGO EL MENOR TIEMPO, DEBE SER DISTINTO A 0
-
-	//SI PRIMERO OCURRE UNA ENTRADA Y HAY SUFICIENTE TIEMPO PARA ELLA RETORNAR
-	return (siguiente_entrada < siguiente_salida && siguiente_entrada < ts);
+	t_salidas[min_position] = siguiente_salida;													//REGISTRO EL NUEVO TIEMPO DE SALIDA
+	siguiente_salida = *min_element(t_salidas.begin(),t_salidas.end(), compare);				//OBTENGO EL MENOR TIEMPO REGISTRADO, SI TODOS SON 0, SE TOMA 0
+		//SI PRIMERO OCURRE UNA ENTRADA Y HAY SUFICIENTE TIEMPO PARA ELLA RETORNAR
+	if (siguiente_salida != 0)
+	{
+		if (siguiente_entrada != 0)
+			return (siguiente_entrada < siguiente_salida);
+	}
+	else
+	{
+		if (siguiente_entrada == 0)
+			return false;
+		else
+			return true;
+	}
 }
 
 void Simulacion::ejecutar( double a, double b, double cr, double tt, double ts)
@@ -88,59 +95,66 @@ void Simulacion::ejecutar( double a, double b, double cr, double tt, double ts)
 	t_llegada = -a*log(r);
 	s_llegada = t_llegada;
 	s_evento = true;
-	//cout << tiempo << "\t" << Nt << "\t" << r << "\t" << t_llegada << "\t0\t0\t" << s_llegada << "\t0\tLlegada" << endl;
+	cout << tiempo << "\t" << Nt << "\t" << r << "\t" << t_llegada << "\t0\t0\t" << s_llegada << "\t0\tLlegada" << endl;
 	tiempo+=t_llegada;
 
 	while( (tiempo<=ts) || (Nt>0) )
 	{
-		if (tiempo >= ts) s_llegada = 0;	//SI YA NO SE ADMITIRAN MAS ENTRADAS POR FALTA DE TIEMPO, EL SIGUIENTE TIEMPO DE LLEGADA DEBE SER NULO
-		//cout << tiempo << "\t";
+		cout << tiempo << "\t";
 		if(s_evento)//SIGUIENTE EVENTO ES UNA ENTRADA
 		{
 			++Nt;																//INCREMENTA LA CANTIDAD DE CLIENTES EN SISTEMA EN 1
-			//cout << Nt << "\t";
+			cout << Nt << "\t";
 			r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);	//GENERAMOS UN NUMERO ALEATORIO PARA LA SIGUIENTE ENTRADA
-			//cout << r << "\t";
 			t_llegada = -a*log(r);												//ESTIMAMOS EL TIEMPO QUE PASARÀ PARA LA SIGUIENTE ENTRADA
-			//cout << t_llegada << "\t";
 			s_llegada = tiempo + t_llegada;										//ESTIMAMOS EL TIEMPO EN SIMULACION QUE OCURRIRA LA LLEGADA
+			if (s_llegada >= ts)												//SI LA SIGUIENTE LLEGADA SOBREPASA EL TIEMPO DE LA SIMULACION, NO SE DEBE CONSIDERAR
+			{
+				r = 0;
+				t_llegada = 0;
+				s_llegada = 0;
+			}
+			cout << r << "\t" << t_llegada << "\t";
 			if(Nt<=4)															//SI HAY SERVIDORES LIBRES PARA ATENDER UNA ENTRADA
 			{
 				r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);	//GENERAMOS UN NUMERO ALEATORIO PARA LA SALIDA
-				//cout << r << "\t";
+				cout << r << "\t";
 				t_servicio = -b*log(r);												//ESTIMAMOS EL TIEMPO EN QUE OCURRIRA LA SALIDA
-				//cout << t_servicio << "\t";
+				cout << t_servicio << "\t";
 				s_salida = tiempo + t_servicio;										//ESTIMAMOS EL TIEMPO EN SIMULACION QUE OCURRIRA LA SALIDA
-				s_evento = comprobarSig_estado(t_salidas,s_salida,s_llegada,ts);	//LLAMAMOS A comprobarSig_estado
+				s_evento = comprobarSig_estado(t_salidas,s_salida,s_llegada,ts, Nt);	//LLAMAMOS A comprobarSig_estado
+			}else{
+				r = 0;
+				t_servicio = 0;
 			}
 		}
-		else //SIGUIENTE EVENTO ES UNA SALIDA
+		else //SIGUIENTE EVENTO ES UNA SALIDA, NO SE GENERA UNA ENTRADA EN ESTA ITERACION
 		{
 			--Nt;
-			//cout << Nt << "\t0\t0\t";
-			if(Nt>0 && Nt>=4) 														//HAY ENTRADAS EN SISTEMA Y AL MENOS UNA ENTRADA NECESITA SER ATENDIDA
+			cout << Nt << "\t0\t0\t";
+			if (Nt >=4)
 			{
 				r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);	//GENERAMOS UN NUMERO ALEATORIO PARA LA SALIDA
-				//cout << r << "\t";
 				t_servicio = -b*log(r);												//ESTIMAMOS EL TIEMPO EN QUE OCURRIRA LA SALIDA
-				//cout << t_servicio << "\t";
 				s_salida = tiempo + t_servicio;										//ESTIMAMOS EL TIEMPO EN SIMULACION QUE OCURRIRA LA SALIDA
+			}else{
+				r = 0;
+				t_servicio = 0;
 			}
-			else{									//NO HAY MAS ENTRADAS PARA SER ATENDIDAS
-				s_salida = 0;						//AL NO HABER OTRA ENTRADA EN ESPERA EL TIEMPO DE SALIDA ES NULO
-			}
-			s_evento = comprobarSig_estado(t_salidas,s_salida,s_llegada,ts);	//LLAMAMOS A comprobarSig_estado
+			cout << r << "\t" << t_servicio << "\t";
+			if (Nt < 4) s_salida = 0;
+			s_evento = comprobarSig_estado(t_salidas,s_salida,s_llegada,ts, Nt);	//LLAMAMOS A comprobarSig_estado
 		}
-		//cout << s_llegada << "\t" << s_salida << "\t";
+		cout << s_llegada << "\t" << s_salida << "\t";
 		if (s_evento)
 		{
-			tiempo += s_llegada;	//SI LA SIGUIENTE ITERACIÓN SERÁ UNA ENTRADA, EL TIEMPO ACTUAL DE SIMULACIÓN INCREMENTA HASTA EL MOMENTO DE LA LLEGADA
-			//cout << "Llegada" << endl;
+			tiempo = s_llegada;	//SI LA SIGUIENTE ITERACIÓN SERÁ UNA ENTRADA, EL TIEMPO ACTUAL DE SIMULACIÓN INCREMENTA HASTA EL MOMENTO DE LA LLEGADA
+			cout << "Llegada" << endl;
 		}
 		else
 		{
-			tiempo += s_salida;			//SI LA SIGUIENTE ITERACIÓN SERÁ UNA SALIDA, EL TIEMPO ACTUAL DE SIMULACIÓN INCREMENTA HASTA EL MOMENTO DE LA SALIDA
-			//cout << "Salida" << endl;
+			tiempo = s_salida;			//SI LA SIGUIENTE ITERACIÓN SERÁ UNA SALIDA, EL TIEMPO ACTUAL DE SIMULACIÓN INCREMENTA HASTA EL MOMENTO DE LA SALIDA
+			cout << "Salida" << endl;
 		}
 	} 
 
--- prueba.cpp
+++ prueba.cpp
@@ -6,21 +6,21 @@ using namespace std;
 
 bool compare(int i, int j)
 {
-	if(i==j && i ==0 && j==0)
-		return 0;
-	else if(i<j)
-		return i;
+	return (i<j && i!=0 && j!=0);
 }
 
 int main ()
 {
 	vector<int> v (4,0);
-	v[0] = 3;
-	v[1] = 4;
-	v[2] = 2;
-	v[3] = 0; 
+	v[0] = 17;
+	v[1] = 2;
+	v[2] = 4;
+	v[3] = 1; 
 	
-	cout << *min_element(v.begin(),v.end(),compare) << endl;
+	cout << min_element(v.begin(),v.end(), compare)-v.begin() << endl <<*min_element(v.begin(),v.end(),compare) << endl;
+	v[min_element(v.begin(),v.end(), compare)-v.begin()] = 0;
+	cout << min_element(v.begin(),v.end(), compare)-v.begin() << endl <<*min_element(v.begin(),v.end(),compare) << endl;
+
 
   return 0;
 }
